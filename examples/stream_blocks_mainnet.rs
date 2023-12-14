babf35e122fe4bbfb7460ece101828d43c66e9e8
GroovieGermanikus <groovie@mango.markets>
2023-12-14 10:58:26 +0100

    ported over grpc_fullstream.rs from lite-rpc commit 54cb41ee

--- examples/stream_blocks_mainnet.rs
+++ examples/stream_blocks_mainnet.rs
@@ -1,19 +1,63 @@
+mod literpc_core_model;
+
+use std::collections::HashMap;
+use std::pin::pin;
+use futures::{Stream, StreamExt};
 use log::{info};
+use solana_sdk::clock::Slot;
 use solana_sdk::commitment_config::CommitmentConfig;
 use tokio::sync::broadcast::{Receiver};
 use tokio::time::{sleep, Duration};
-use yellowstone_grpc_proto::geyser::{CommitmentLevel, SubscribeUpdateBlock};
-use geyser_grpc_connector::grpcmultiplex_fastestwins::{create_multiplex, GrpcSourceConfig};
+use yellowstone_grpc_proto::geyser::{CommitmentLevel, SubscribeRequestFilterBlocks, SubscribeRequestFilterBlocksMeta, SubscribeUpdate, SubscribeUpdateBlock};
+use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
+use geyser_grpc_connector::grpcmultiplex_fastestwins::{create_multiplex, ExtractBlockFromStream, GrpcSourceConfig};
+use crate::literpc_core_model::{map_produced_block, ProducedBlock};
 
-fn start_example_consumer(blocks_notifier: Receiver<Box<SubscribeUpdateBlock>>) {
+fn start_example_consumer(mut block_stream: impl Stream<Item=ProducedBlock> + Send + 'static) {
     tokio::spawn(async move {
-        let mut blocks_notifier = blocks_notifier;
-        loop {
-            let block = blocks_notifier.recv().await.unwrap();
-            info!("received block #{} with {} txs", block.slot, block.transactions.len());
+        let mut block_stream = pin!(block_stream);
+        while let Some(block) = block_stream.next().await {
+            info!("received block #{}", block.slot,);
         }
     });
 }
+
+
+struct ExtractBlock(CommitmentConfig);
+impl ExtractBlockFromStream for ExtractBlock {
+    type Block = ProducedBlock;
+    fn extract(&self, update: SubscribeUpdate, current_slot: Slot) -> Option<(Slot, Self::Block)> {
+        match update.update_oneof {
+            Some(UpdateOneof::Block(update_block_message))
+            if update_block_message.slot > current_slot =>
+                {
+                    let block = map_produced_block(update_block_message, self.0);
+                    Some((block.slot, block))
+                }
+            _ => None,
+        }
+    }
+
+    fn get_block_subscription_filter(&self) -> HashMap<String, SubscribeRequestFilterBlocks> {
+        let mut blocks_subs = HashMap::new();
+        blocks_subs.insert(
+            "client".to_string(),
+            SubscribeRequestFilterBlocks {
+                account_include: Default::default(),
+                include_transactions: Some(true),
+                include_accounts: Some(false),
+                include_entries: Some(false),
+            },
+        );
+        blocks_subs
+    }
+    fn get_blockmeta_subscription_filter(
+        &self,
+    ) -> HashMap<String, SubscribeRequestFilterBlocksMeta> {
+        HashMap::new()
+    }
+}
+
 
 #[tokio::main]
 pub async fn main() {
@@ -30,18 +74,17 @@ pub async fn main() {
     // testnet - NOTE: this connection has terrible lags (almost 5 minutes)
     // let grpc_addr = "http://147.28.169.13:10000".to_string();
 
-    let (block_sx, blocks_notifier) = tokio::sync::broadcast::channel(1000);
 
     let green_config = GrpcSourceConfig::new("triton".to_string(), grpc_addr_mainnet_triton, None);
     let blue_config = GrpcSourceConfig::new("mangoams81".to_string(), grpc_addr_mainnet_ams81, None);
     let toxiproxy_config = GrpcSourceConfig::new("toxiproxy".to_string(), grpc_addr_mainnet_triton_toxi, None);
 
-    create_multiplex(
+    let multiplex_stream = create_multiplex(
         vec![green_config, blue_config, toxiproxy_config],
         CommitmentConfig::finalized(),
-        block_sx).await;
+        ExtractBlock(CommitmentConfig::confirmed()),);
 
-    start_example_consumer(blocks_notifier);
+    start_example_consumer(multiplex_stream);
 
     // "infinite" sleep
     sleep(Duration::from_secs(1800)).await;
--- src/grpcmultiplex_fastestwins.rs
+++ src/grpcmultiplex_fastestwins.rs
@@ -1,111 +1,96 @@
-use std::collections::{HashMap};
-use std::ops::{Add};
-use anyhow::{Context};
 use async_stream::stream;
 use futures::{Stream, StreamExt};
-use itertools::{Itertools};
-use log::{debug, info, warn};
+use itertools::Itertools;
+use log::{info, warn};
 use solana_sdk::clock::Slot;
 use solana_sdk::commitment_config::CommitmentConfig;
-use solana_sdk::commitment_config::CommitmentLevel;
-use tokio::{select};
-use tokio::sync::broadcast::{Sender};
-use tokio::time::{Duration, Instant, sleep_until};
-use yellowstone_grpc_client::GeyserGrpcClient;
-use yellowstone_grpc_proto::geyser::{SubscribeRequestFilterBlocks, SubscribeUpdate, SubscribeUpdateBlock};
+use std::collections::HashMap;
+use std::pin::pin;
+use tokio::task::JoinHandle;
+use tokio::time::{sleep, Duration};
+use yellowstone_grpc_client::{GeyserGrpcClient, GeyserGrpcClientResult};
 use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
+use yellowstone_grpc_proto::geyser::SubscribeUpdateBlockMeta;
+use yellowstone_grpc_proto::geyser::{
+    CommitmentLevel, SubscribeRequestFilterBlocks, SubscribeUpdate,
+};
+use yellowstone_grpc_proto::prelude::SubscribeRequestFilterBlocksMeta;
 use yellowstone_grpc_proto::tonic::transport::ClientTlsConfig;
+use yellowstone_grpc_proto::tonic::Status;
 
-// use solana_lite_rpc_cluster_endpoints::grpc_subscription::{create_block_processing_task, map_produced_block};
-// use solana_lite_rpc_core::AnyhowJoinHandle;
-// use solana_lite_rpc_core::structures::produced_block::ProducedBlock;
+pub trait ExtractBlockFromStream {
+    type Block;
+    fn extract(&self, update: SubscribeUpdate, current_slot: Slot) -> Option<(Slot, Self::Block)>;
+    fn get_block_subscription_filter(&self) -> HashMap<String, SubscribeRequestFilterBlocks>;
+    fn get_blockmeta_subscription_filter(
+        &self,
+    ) -> HashMap<String, SubscribeRequestFilterBlocksMeta>;
+}
 
-// TODO map SubscribeUpdateBlock
-pub async fn create_multiplex(
+struct ExtractBlock(CommitmentConfig);
+
+struct ExtractBlockMeta(CommitmentConfig);
+
+
+pub fn create_multiplex<E>(
     grpc_sources: Vec<GrpcSourceConfig>,
     commitment_config: CommitmentConfig,
-    block_sx: Sender<Box<SubscribeUpdateBlock>>,
-) -> tokio::task::JoinHandle<anyhow::Result<()>> {
+    extractor: E,
+) -> impl Stream<Item = E::Block>
+    where
+        E: ExtractBlockFromStream,
+{
     assert!(
         commitment_config == CommitmentConfig::confirmed()
             || commitment_config == CommitmentConfig::finalized(),
         "Only CONFIRMED and FINALIZED is supported");
     // note: PROCESSED blocks are not sequential in presense of forks; this will break the logic
 
-    if grpc_sources.is_empty() {
+    if grpc_sources.len() < 1 {
         panic!("Must have at least one source");
     }
 
-    tokio::spawn(async move {
-        info!("Starting multiplexer with {} sources: {}",
-            grpc_sources.len(),
-            grpc_sources.iter().map(|source| source.label.clone()).join(", "));
+    info!(
+        "Starting multiplexer with {} sources: {}",
+        grpc_sources.len(),
+        grpc_sources
+            .iter()
+            .map(|source| source.label.clone())
+            .join(", ")
+    );
 
-        let mut futures = futures::stream::SelectAll::new();
-        for grpc_source in grpc_sources {
-            // note: stream never terminates
-            let stream = create_geyser_reconnecting_stream(grpc_source.clone(), commitment_config).await;
-            futures.push(Box::pin(stream));
-        }
+    let mut futures = futures::stream::SelectAll::new();
 
-        let mut current_slot: Slot = 0;
-
-        'main_loop: loop {
-
-            let block_cmd = select! {
-                message = futures.next() => {
-                    match message {
-                        Some(message) => {
-                            map_filter_block_message(current_slot, message, commitment_config)
-                        }
-                        None => {
-                            panic!("source stream is not supposed to terminate");
-                        }
-                    }
-                }
-            };
-
-            match block_cmd {
-                BlockCmd::ForwardBlock(block) => {
-                    current_slot = block.slot;
-                    block_sx.send(block).context("send block to downstream")?;
-                }
-                BlockCmd::DiscardBlockBehindTip(slot) => {
-                    debug!(". discarding redundant block #{}", slot);
-                }
-                BlockCmd::SkipMessage => {
-                    debug!(". skipping this message by type");
-                }
-            }
-
-        }
-    })
-}
-
-// look Ma, no Clone!
-#[derive(Debug)]
-enum BlockCmd {
-    ForwardBlock(Box<SubscribeUpdateBlock>),
-    DiscardBlockBehindTip(Slot),
-    // skip geyser messages which are not block related updates
-    SkipMessage,
-}
-
-fn map_filter_block_message(current_slot: Slot, update_message: SubscribeUpdate, _commitment_config: CommitmentConfig) -> BlockCmd {
-    if let Some(UpdateOneof::Block(update_block_message)) = update_message.update_oneof {
-        if update_block_message.slot <= current_slot && current_slot != 0 {
-            // no progress - skip this
-            return BlockCmd::DiscardBlockBehindTip(update_block_message.slot);
-        }
-
-        // expensive
-        // let produced_block = map_produced_block(update_block_message, commitment_config);
-
-        BlockCmd::ForwardBlock(Box::new(update_block_message))
-    } else {
-        BlockCmd::SkipMessage
+    for grpc_source in grpc_sources {
+        futures.push(Box::pin(create_geyser_reconnecting_stream(
+            grpc_source.clone(),
+            (
+                extractor.get_block_subscription_filter(),
+                extractor.get_blockmeta_subscription_filter(),
+            ),
+            commitment_config,
+        )));
     }
 
+    filter_blocks(futures, extractor)
+}
+
+fn filter_blocks<S, E>(geyser_stream: S, extractor: E) -> impl Stream<Item = E::Block>
+    where
+        S: Stream<Item = Option<SubscribeUpdate>>,
+        E: ExtractBlockFromStream,
+{
+    let mut current_slot: Slot = 0;
+    stream! {
+        for await update in geyser_stream {
+            if let Some(update) = update {
+                if let Some((new_slot, block)) = extractor.extract(update, current_slot) {
+                    current_slot = new_slot;
+                    yield block;
+                }
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -128,11 +113,24 @@ impl GrpcSourceConfig {
     }
 }
 
+enum ConnectionState<S: Stream<Item = Result<SubscribeUpdate, Status>>> {
+    NotConnected,
+    Connecting(JoinHandle<GeyserGrpcClientResult<S>>),
+    Ready(S),
+    WaitReconnect,
+}
+
 // TODO use GrpcSource
 // note: stream never terminates
-async fn create_geyser_reconnecting_stream(
+fn create_geyser_reconnecting_stream(
     grpc_source: GrpcSourceConfig,
-    commitment_config: CommitmentConfig) -> impl Stream<Item = SubscribeUpdate> {
+    blocks_filters: (
+        HashMap<String, SubscribeRequestFilterBlocks>,
+        HashMap<String, SubscribeRequestFilterBlocksMeta>,
+    ),
+    commitment_config: CommitmentConfig,
+) -> impl Stream<Item = Option<SubscribeUpdate>> {
+    let label = grpc_source.label.clone();
 
     // solana_sdk -> yellowstone
     let commitment_level = match commitment_config.commitment {
@@ -141,75 +139,95 @@ async fn create_geyser_reconnecting_stream(
         _ => panic!("Only CONFIRMED and FINALIZED is supported/suggested"),
     };
 
-    let label = grpc_source.label.clone();
+    // NOT_CONNECTED; CONNECTING
+    let mut state = ConnectionState::NotConnected;
+
+    // in case of cancellation, we restart from here:
+    // thus we want to keep the progression in a state object outside the stream! makro
     stream! {
-        let mut throttle_barrier = Instant::now();
-        'reconnect_loop: loop {
-            sleep_until(throttle_barrier).await;
-            throttle_barrier = Instant::now().add(Duration::from_millis(1000));
+        loop{
+            let yield_value;
+            (state, yield_value) = match state {
+                ConnectionState::NotConnected => {
 
-            let connect_result = GeyserGrpcClient::connect_with_timeout(
-                grpc_source.grpc_addr.clone(), grpc_source.grpc_x_token.clone(), grpc_source.tls_config.clone(),
-                Some(Duration::from_secs(2)), Some(Duration::from_secs(2)), false).await;
+                    let connection_task = tokio::spawn({
+                        let addr = grpc_source.grpc_addr.clone();
+                        let token = grpc_source.grpc_x_token.clone();
+                        let config = grpc_source.tls_config.clone();
+                        let (block_filter, blockmeta_filter) = blocks_filters.clone();
+                        async move {
 
-            let mut client = match connect_result {
-                Ok(connected_client) => connected_client,
-                Err(geyser_grpc_client_error) => {
-                    // TODO identify non-recoverable errors and cancel stream
-                    warn!("Connect failed on {} - retrying: {:?}", label, geyser_grpc_client_error);
-                    continue 'reconnect_loop;
+                            let connect_result = GeyserGrpcClient::connect_with_timeout(
+                                addr, token, config,
+                                Some(Duration::from_secs(2)), Some(Duration::from_secs(2)), false).await;
+                            let mut client = connect_result?;
+
+                            // Connected;
+                            let subscribe_result = client
+                                .subscribe_once(
+                                    HashMap::new(),
+                                    Default::default(),
+                                    HashMap::new(),
+                                    Default::default(),
+                                    block_filter,
+                                    blockmeta_filter,
+                                    Some(commitment_level),
+                                    Default::default(),
+                                    None,
+                                ).await;
+
+                            subscribe_result
+                        }
+                    });
+
+                    (ConnectionState::Connecting(connection_task), None)
                 }
-            };
+                ConnectionState::Connecting(connection_task) => {
+                    let subscribe_result = connection_task.await;
 
-            let mut blocks_subs = HashMap::new();
-            blocks_subs.insert(
-                "client".to_string(),
-                SubscribeRequestFilterBlocks {
-                    account_include: Default::default(),
-                    include_transactions: Some(true),
-                    include_accounts: Some(false),
-                    include_entries: Some(false),
-                },
-            );
-
-            let subscribe_result = client
-                .subscribe_once(
-                    HashMap::new(),
-                    Default::default(),
-                    HashMap::new(),
-                    Default::default(),
-                    blocks_subs,
-                    Default::default(),
-                    Some(commitment_level),
-                    Default::default(),
-                    None,
-                ).await;
-
-            let geyser_stream = match subscribe_result {
-                Ok(subscribed_stream) => subscribed_stream,
-                Err(geyser_grpc_client_error) => {
-                    // TODO identify non-recoverable errors and cancel stream
-                    warn!("Subscribe failed on {} - retrying: {:?}", label, geyser_grpc_client_error);
-                    continue 'reconnect_loop;
-                }
-            };
-
-            for await update_message in geyser_stream {
-                match update_message {
-                    Ok(update_message) => {
-                        info!(">message on {}", label);
-                        yield update_message;
+                     match subscribe_result {
+                        Ok(Ok(subscribed_stream)) => (ConnectionState::Ready(subscribed_stream), None),
+                        Ok(Err(geyser_error)) => {
+                             // TODO identify non-recoverable errors and cancel stream
+                            warn!("Subscribe failed on {} - retrying: {:?}", label, geyser_error);
+                            (ConnectionState::WaitReconnect, None)
+                        },
+                        Err(geyser_grpc_task_error) => {
+                            panic!("Task aborted - should not happen :{geyser_grpc_task_error}");
+                        }
                     }
-                    Err(tonic_status) => {
-                        // TODO identify non-recoverable errors and cancel stream
-                        warn!("Receive error on {} - retrying: {:?}", label, tonic_status);
-                        continue 'reconnect_loop;
-                    }
-                }
-            } // -- production loop
 
-            warn!("stream consumer loop {} terminated", label);
-        } // -- main loop
+                }
+                ConnectionState::Ready(mut geyser_stream) => {
+
+                    //for await update_message in geyser_stream {
+                        match geyser_stream.next().await {
+                            Some(Ok(update_message)) => {
+                                info!(">message on {}", label);
+                                (ConnectionState::Ready(geyser_stream), Some(update_message))
+                            }
+                            Some(Err(tonic_status)) => {
+                                // TODO identify non-recoverable errors and cancel stream
+                                warn!("Receive error on {} - retrying: {:?}", label, tonic_status);
+                                (ConnectionState::WaitReconnect, None)
+                            }
+                            None =>  {
+                                //TODO should not arrive. Mean the stream close.
+                                warn!("Geyzer stream close on {} - retrying", label);
+                                (ConnectionState::WaitReconnect, None)
+                            }
+                        }
+                    //} // -- production loop
+
+                }
+                ConnectionState::WaitReconnect => {
+                    // TODO implement backoff
+                    sleep(Duration::from_secs(1)).await;
+                    (ConnectionState::NotConnected, None)
+                }
+            }; // -- match
+            yield yield_value
+        }
+
     } // -- stream!
-
 }
