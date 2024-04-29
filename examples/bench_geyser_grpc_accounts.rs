f280f903d7e2792da1dc96925c1d4e3fa498b8cb
GroovieGermanikus <groovie@mango.markets>
2024-04-29 09:12:53 +0200

    cleanup

--- examples/bench_geyser_grpc_accounts.rs
+++ examples/bench_geyser_grpc_accounts.rs
@@ -17,87 +17,6 @@ use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
 use yellowstone_grpc_proto::geyser::SubscribeUpdate;
 use yellowstone_grpc_proto::prost::Message as _;
 
-fn start_example_account_consumer(mut multiplex_channel: Receiver<Message>) {
-    tokio::spawn(async move {
-        loop {
-            match multiplex_channel.recv().await {
-                Some(Message::GeyserSubscribeUpdate(update)) => match update.update_oneof {
-                    Some(UpdateOneof::Account(update)) => {
-                        let account_info = update.account.unwrap();
-                        let account_pk = Pubkey::try_from(account_info.pubkey).unwrap();
-                        info!(
-                            "got account update (green)!!! {} - {:?} - {} bytes",
-                            update.slot,
-                            account_pk,
-                            account_info.data.len()
-                        );
-                        let bytes: [u8; 32] = account_pk.to_bytes();
-                    }
-                    None => {}
-                    _ => {}
-                },
-                None => {
-                    log::warn!("multiplexer channel closed - aborting");
-                    return;
-                }
-                Some(Message::Connecting(_)) => {}
-            }
-        }
-    });
-}
-
-#[allow(dead_code)]
-fn start_example_blockmini_consumer(
-    multiplex_stream: impl Stream<Item = BlockMini> + Send + 'static,
-) {
-    tokio::spawn(async move {
-        let mut blockmeta_stream = pin!(multiplex_stream);
-        while let Some(mini) = blockmeta_stream.next().await {
-            info!(
-                "emitted block mini #{}@{} with {} bytes from multiplexer",
-                mini.slot, mini.commitment_config.commitment, mini.blocksize
-            );
-        }
-    });
-}
-
-pub struct BlockMini {
-    pub blocksize: usize,
-    pub slot: Slot,
-    pub commitment_config: CommitmentConfig,
-}
-
-struct BlockMiniExtractor(CommitmentConfig);
-
-impl FromYellowstoneExtractor for BlockMiniExtractor {
-    type Target = BlockMini;
-    fn map_yellowstone_update(&self, update: SubscribeUpdate) -> Option<(Slot, Self::Target)> {
-        match update.update_oneof {
-            Some(UpdateOneof::Block(update_block_message)) => {
-                let blocksize = update_block_message.encoded_len();
-                let slot = update_block_message.slot;
-                let mini = BlockMini {
-                    blocksize,
-                    slot,
-                    commitment_config: self.0,
-                };
-                Some((slot, mini))
-            }
-            Some(UpdateOneof::BlockMeta(update_blockmeta_message)) => {
-                let blocksize = update_blockmeta_message.encoded_len();
-                let slot = update_blockmeta_message.slot;
-                let mini = BlockMini {
-                    blocksize,
-                    slot,
-                    commitment_config: self.0,
-                };
-                Some((slot, mini))
-            }
-            _ => None,
-        }
-    }
-}
-
 #[tokio::main]
 pub async fn main() {
     // RUST_LOG=info,stream_blocks_mainnet=debug,geyser_grpc_connector=trace
@@ -134,8 +53,38 @@ pub async fn main() {
         exit_notify.resubscribe(),
     );
 
-    start_example_account_consumer(accounts_rx);
+    start_tracking_account_consumer(accounts_rx);
 
     // "infinite" sleep
     sleep(Duration::from_secs(1800)).await;
+}
+
+
+fn start_tracking_account_consumer(mut multiplex_channel: Receiver<Message>) {
+    tokio::spawn(async move {
+        loop {
+            match multiplex_channel.recv().await {
+                Some(Message::GeyserSubscribeUpdate(update)) => match update.update_oneof {
+                    Some(UpdateOneof::Account(update)) => {
+                        let account_info = update.account.unwrap();
+                        let account_pk = Pubkey::try_from(account_info.pubkey).unwrap();
+                        info!(
+                            "got account update (green)!!! {} - {:?} - {} bytes",
+                            update.slot,
+                            account_pk,
+                            account_info.data.len()
+                        );
+                        let bytes: [u8; 32] = account_pk.to_bytes();
+                    }
+                    None => {}
+                    _ => {}
+                },
+                None => {
+                    log::warn!("multiplexer channel closed - aborting");
+                    return;
+                }
+                Some(Message::Connecting(_)) => {}
+            }
+        }
+    });
 }
