a57860ec14f32b91f5bfebaa542c94c17ff764b5
GroovieGermanikus <groovie@mango.markets>
2024-06-06 11:06:56 +0200

    calculate vote timestamp spread

--- examples/stream_vote_transactions.rs
+++ examples/stream_vote_transactions.rs
@@ -1,13 +1,14 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use futures::{Stream, StreamExt};
 use log::{debug, info, trace};
-use solana_sdk::clock::Slot;
+use solana_sdk::clock::{Slot, UnixTimestamp};
 use solana_sdk::commitment_config::CommitmentConfig;
 use std::env;
 use std::pin::pin;
 use std::str::FromStr;
 use std::sync::Arc;
 use dashmap::DashMap;
+use itertools::Itertools;
 use solana_account_decoder::parse_token::{parse_token, spl_token_ids, TokenAccountType, UiTokenAccount};
 use solana_account_decoder::parse_token::UiAccountState::Initialized;
 use solana_sdk::program_utils::limited_deserialize;
@@ -24,6 +25,7 @@ use tracing::warn;
 use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
 use yellowstone_grpc_proto::geyser::{SubscribeRequest, SubscribeRequestFilterAccounts, SubscribeRequestFilterSlots, SubscribeRequestFilterTransactions, SubscribeUpdate};
 use yellowstone_grpc_proto::prost::Message as _;
+use geyser_grpc_connector::histogram_percentiles::calculate_percentiles;
 
 
 const ENABLE_TIMESTAMP_TAGGING: bool = false;
@@ -60,6 +62,9 @@ pub async fn main() {
     );
 
     tokio::spawn(async move {
+
+        let mut vote_times_by_slot: HashMap<Slot, HashSet<UnixTimestamp>> = HashMap::new();
+
         let mut green_stream = pin!(green_stream);
         while let Some(message) = green_stream.next().await {
             match message {
@@ -68,23 +73,23 @@ pub async fn main() {
                         Some(UpdateOneof::Transaction(update)) => {
 
                             let message = update.transaction.unwrap().transaction.unwrap().message.unwrap();
+                            let slot = update.slot;
 
 
                             // https://docs.solanalabs.com/implemented-proposals/validator-timestamp-oracle
                             for ci in message.instructions {
                                 let vote_instruction = limited_deserialize::<VoteInstruction>(&ci.data).unwrap();
-                                info!("vote_instruction: {:?}", vote_instruction);
+                                let last_voted_slot = vote_instruction.last_voted_slot().unwrap();
                                 info!("vote_instruction: {:?}", vote_instruction.timestamp().unwrap());
+                                vote_times_by_slot.entry(last_voted_slot).or_insert(HashSet::new()).insert(vote_instruction.timestamp().unwrap());
                             }
 
-                            // let is_vote_transaction = message.instructions().iter().any(|i| {
-                            //     i.program_id(message.static_account_keys())
-                            //         .eq(&solana_sdk::vote::program::id())
-                            //         && limited_deserialize::<VoteInstruction>(&i.data)
-                            //         .map(|vi| vi.is_simple_vote())
-                            //         .unwrap_or(false)
-                            // });
 
+                            // hack to look at reasonable settled slot
+                            // print_spread(&vote_times_by_slot, slot);
+                            if vote_times_by_slot.contains_key(&(slot-10)) {
+                                print_spread(&vote_times_by_slot, slot - 10);
+                            }
 
 
                         }
@@ -102,6 +107,14 @@ pub async fn main() {
 
     // "infinite" sleep
     sleep(Duration::from_secs(1800)).await;
+}
+
+fn print_spread(vote_times_by_slot: &HashMap<Slot, HashSet<UnixTimestamp>>, slot: Slot) {
+    let slots = vote_times_by_slot.get(&slot).unwrap();
+    let min_slot = slots.iter().min().unwrap();
+    let array = slots.iter().sorted().map(|x| (*x - min_slot) as f64).collect_vec();
+    let histo = calculate_percentiles(&array);
+    info!("slot: {} histo: {}", slot, histo);
 }
 
 pub fn transaction_filter() -> SubscribeRequest {
