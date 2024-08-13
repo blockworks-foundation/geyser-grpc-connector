d46aba194644001d084101a4ac8493d82d647f7a
GroovieGermanikus <groovie@mango.markets>
2024-08-13 23:08:17 +0200

    fmt+clippy

--- examples/dump_slots_stream_samples.rs
+++ examples/dump_slots_stream_samples.rs
@@ -6,7 +6,6 @@
 /// - only finalized accounts
 ///
 /// we want to see if there is a difference in timing of "processed accounts" in the mix with slot vs "only processed accounts"
-
 use log::{info, warn};
 use solana_sdk::clock::Slot;
 use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
@@ -31,11 +30,15 @@ use solana_sdk::pubkey::Pubkey;
 use solana_sdk::signature::Signature;
 use solana_sdk::transaction::TransactionError;
 use tokio::sync::mpsc::Receiver;
-use yellowstone_grpc_proto::geyser::{SubscribeRequest, SubscribeRequestFilterSlots, SubscribeUpdateSlot};
+use yellowstone_grpc_proto::geyser::{
+    SubscribeRequest, SubscribeRequestFilterSlots, SubscribeUpdateSlot,
+};
 
 use geyser_grpc_connector::grpc_subscription_autoreconnect_tasks::create_geyser_autoconnection_task_with_mpsc;
 use geyser_grpc_connector::grpcmultiplex_fastestwins::FromYellowstoneExtractor;
-use geyser_grpc_connector::{GeyserFilter, GrpcConnectionTimeouts, GrpcSourceConfig, map_commitment_level, Message};
+use geyser_grpc_connector::{
+    map_commitment_level, GeyserFilter, GrpcConnectionTimeouts, GrpcSourceConfig, Message,
+};
 use tokio::time::{sleep, Duration};
 use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
 use yellowstone_grpc_proto::geyser::SubscribeUpdate;
@@ -47,27 +50,45 @@ fn start_all_slots_and_processed_accounts_consumer(mut slots_channel: Receiver<M
             match slots_channel.recv().await {
                 Some(Message::GeyserSubscribeUpdate(update)) => match update.update_oneof {
                     Some(UpdateOneof::Slot(update_slot)) => {
-                        let since_epoch_ms = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+                        let since_epoch_ms = SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis();
 
                         let short_status = match map_slot_status(&update_slot) {
                             CommitmentLevel::Processed => "P",
                             CommitmentLevel::Confirmed => "C",
                             CommitmentLevel::Finalized => "F",
-                            _ => { panic!("unexpected commitment level") }
+                            _ => {
+                                panic!("unexpected commitment level")
+                            }
                         };
                         // DUMPSLOT 283356662,283356661,F,1723556492340
-                        info!("MIXSLOT {},{:09},{},{}", update_slot.slot, update_slot.parent.unwrap_or(0), short_status, since_epoch_ms);
+                        info!(
+                            "MIXSLOT {},{:09},{},{}",
+                            update_slot.slot,
+                            update_slot.parent.unwrap_or(0),
+                            short_status,
+                            since_epoch_ms
+                        );
                     }
                     Some(UpdateOneof::Account(update_account)) => {
-                        let since_epoch_ms = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+                        let since_epoch_ms = SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis();
 
                         let account_info = update_account.account.unwrap();
                         let slot = update_account.slot;
-                        let account_pk = Pubkey::new_from_array(account_info.pubkey.try_into().unwrap());
+                        let account_pk =
+                            Pubkey::new_from_array(account_info.pubkey.try_into().unwrap());
                         let write_version = account_info.write_version;
                         let data_len = account_info.data.len();
                         // DUMPACCOUNT 283417593,HTQeo4GNbZfGY5G4fAkDr1S5xnz5qWXFgueRwgw53aU1,1332997857270,752,1723582355872
-                        info!("MIXACCOUNT {},{},{},{},{}", slot, account_pk, write_version, data_len, since_epoch_ms);
+                        info!(
+                            "MIXACCOUNT {},{},{},{},{}",
+                            slot, account_pk, write_version, data_len, since_epoch_ms
+                        );
                     }
                     None => {}
                     _ => {}
@@ -82,19 +103,25 @@ fn start_all_slots_and_processed_accounts_consumer(mut slots_channel: Receiver<M
     });
 }
 
-
 // need to provide the commitment level used to filter the accounts
-fn start_account_same_level(mut slots_channel: Receiver<Message>, commitment_level: CommitmentLevel) {
+fn start_account_same_level(
+    mut slots_channel: Receiver<Message>,
+    commitment_level: CommitmentLevel,
+) {
     tokio::spawn(async move {
         loop {
             match slots_channel.recv().await {
                 Some(Message::GeyserSubscribeUpdate(update)) => match update.update_oneof {
                     Some(UpdateOneof::Account(update_account)) => {
-                        let since_epoch_ms = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+                        let since_epoch_ms = SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis();
 
                         let account_info = update_account.account.unwrap();
                         let slot = update_account.slot;
-                        let account_pk = Pubkey::new_from_array(account_info.pubkey.try_into().unwrap());
+                        let account_pk =
+                            Pubkey::new_from_array(account_info.pubkey.try_into().unwrap());
                         let write_version = account_info.write_version;
                         let data_len = account_info.data.len();
 
@@ -102,11 +129,16 @@ fn start_account_same_level(mut slots_channel: Receiver<Message>, commitment_lev
                             CommitmentLevel::Processed => "P",
                             CommitmentLevel::Confirmed => "C",
                             CommitmentLevel::Finalized => "F",
-                            _ => { panic!("unexpected commitment level") }
+                            _ => {
+                                panic!("unexpected commitment level")
+                            }
                         };
 
                         // DUMPACCOUNT 283417593,HTQeo4GNbZfGY5G4fAkDr1S5xnz5qWXFgueRwgw53aU1,1332997857270,752,1723582355872
-                        info!("DUMPACCOUNT {},{},{},{},{},{}", slot, short_status, account_pk, write_version, data_len, since_epoch_ms);
+                        info!(
+                            "DUMPACCOUNT {},{},{},{},{},{}",
+                            slot, short_status, account_pk, write_version, data_len, since_epoch_ms
+                        );
                     }
                     None => {}
                     _ => {}
@@ -121,16 +153,19 @@ fn start_account_same_level(mut slots_channel: Receiver<Message>, commitment_lev
     });
 }
 
-fn map_slot_status(slot_update: &SubscribeUpdateSlot) -> solana_sdk::commitment_config::CommitmentLevel {
-    use yellowstone_grpc_proto::geyser::CommitmentLevel as yCL;
+fn map_slot_status(
+    slot_update: &SubscribeUpdateSlot,
+) -> solana_sdk::commitment_config::CommitmentLevel {
     use solana_sdk::commitment_config::CommitmentLevel as solanaCL;
-    yellowstone_grpc_proto::geyser::CommitmentLevel::try_from(slot_update.status).map(|v| match v {
-        yCL::Processed => solanaCL::Processed,
-        yCL::Confirmed => solanaCL::Confirmed,
-        yCL::Finalized => solanaCL::Finalized,
-    }).expect("valid commitment level")
+    use yellowstone_grpc_proto::geyser::CommitmentLevel as yCL;
+    yellowstone_grpc_proto::geyser::CommitmentLevel::try_from(slot_update.status)
+        .map(|v| match v {
+            yCL::Processed => solanaCL::Processed,
+            yCL::Confirmed => solanaCL::Confirmed,
+            yCL::Finalized => solanaCL::Finalized,
+        })
+        .expect("valid commitment level")
 }
-
 
 #[tokio::main]
 pub async fn main() {
@@ -154,7 +189,6 @@ pub async fn main() {
 
     let green_config =
         GrpcSourceConfig::new(grpc_addr_green, grpc_x_token_green, None, timeouts.clone());
-
 
     // mix of (all) slots and processed accounts
     let (autoconnect_tx, slots_accounts_rx) = tokio::sync::mpsc::channel(10);
@@ -189,7 +223,6 @@ pub async fn main() {
     start_account_same_level(only_processed_accounts_rx, CommitmentLevel::Processed);
     start_account_same_level(only_confirmed_accounts_rx, CommitmentLevel::Confirmed);
     start_account_same_level(only_finalized_accounts_rx, CommitmentLevel::Finalized);
-
 
     // "infinite" sleep
     sleep(Duration::from_secs(3600 * 5)).await;
@@ -226,7 +259,6 @@ fn all_slots_and_processed_accounts_together() -> SubscribeRequest {
     }
 }
 
-
 fn accounts_at_level(commitment_level: CommitmentLevel) -> SubscribeRequest {
     let mut account_subs = HashMap::new();
     account_subs.insert(
@@ -241,7 +273,9 @@ fn accounts_at_level(commitment_level: CommitmentLevel) -> SubscribeRequest {
     SubscribeRequest {
         accounts: account_subs,
         ping: None,
-        commitment: Some(map_commitment_level(CommitmentConfig { commitment: commitment_level }) as i32),
+        commitment: Some(map_commitment_level(CommitmentConfig {
+            commitment: commitment_level,
+        }) as i32),
         ..Default::default()
     }
 }
@@ -251,14 +285,17 @@ fn parse_output() {
     let data = "283360248,000000000,C,1723558000558";
     let mut rdr = ReaderBuilder::new()
         .has_headers(false)
-        .from_reader(data. as_bytes());
+        .from_reader(data.as_bytes());
 
     let all_records = rdr.records().collect_vec();
     assert_eq!(1, all_records.len());
     let record = all_records[0].as_ref().unwrap();
 
     let slot: u64 = record[0].parse().unwrap();
-    let parent: Option<u64> = record[1].parse().ok().and_then(|v| if v == 0 { None } else { Some(v) });
+    let parent: Option<u64> = record[1]
+        .parse()
+        .ok()
+        .and_then(|v| if v == 0 { None } else { Some(v) });
     let status = match record[2].to_string().as_str() {
         "P" => CommitmentLevel::Processed,
         "C" => CommitmentLevel::Confirmed,
