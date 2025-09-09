b6bc49a0e4eb3dd848aee076ddf40f1896d494f6
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-09-09 10:27:18 +0200

    move blacklist into subscriptio

--- examples/analyze_blacklist_transactions.rs
+++ examples/analyze_blacklist_transactions.rs
@@ -8,7 +8,7 @@ use log::{info, trace};
 use solana_account_decoder::parse_token::spl_token_ids;
 use solana_sdk::clock::{Slot, UnixTimestamp};
 use solana_sdk::pubkey::Pubkey;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::str::FromStr;
 use std::sync::atomic::AtomicU64;
@@ -59,14 +59,7 @@ pub async fn main() {
 
     let _jh = create_geyser_autoconnection_task_with_mpsc(
         config.clone(),
-        all_transactions(),
-        autoconnect_tx.clone(),
-        exit_rx.resubscribe(),
-    );
-
-    let _jh = create_geyser_autoconnection_task_with_mpsc(
-        config.clone(),
-        blacklisted_transactions(),
+        filter_transactions(),
         autoconnect_tx.clone(),
         exit_rx.resubscribe(),
     );
@@ -87,36 +80,71 @@ fn start_tracking_account_consumer(
 
         let mut tx_count_total: u64 = 0;
         let mut tx_count_blacklisted: u64 = 0;
+        let mut tx_count_phoenix: u64 = 0;
+
+        let mut blacklisted_by_account: HashMap<Pubkey, u64> = HashMap::new();
 
         let mut last_prune: Slot = 0;
         let mut last_print: Slot = 0;
+
+        let blacklist: HashSet<Pubkey> = blacklist()
+            .iter()
+            .filter_map(|s| Pubkey::from_str(s).ok())
+            .collect();
 
         'stream_loop: loop {
             match geyser_messages_rx.recv().await {
                 Some(Message::GeyserSubscribeUpdate(update)) => {
                     match update.update_oneof {
                         Some(UpdateOneof::Transaction(update_tx)) => {
-                            assert_eq!(update.filters.len(), 1);
-                            let filter = update.filters[0].to_ascii_lowercase();
-                            let is_blacklist = filter == "transaction_sub_blacklisted";
+                            let filters = update.filters.iter().map(|s| s.to_ascii_lowercase()).collect_vec();
+                            let is_blacklist = filters.contains(&"transaction_sub_blacklisted".to_string());
+                            let is_phoenix = filters.contains(&"transaction_sub_phoenix".to_string());
                             let slot = update_tx.slot;
-                            let tx_sig = bs58::encode(update_tx.transaction.unwrap().signature).into_string();
+                            let tx_info = update_tx.transaction.unwrap();
+                            let tx_sig = bs58::encode(tx_info.signature).into_string();
+                            let account_keys: Vec<Pubkey> = tx_info.transaction.unwrap().message.unwrap()
+                                .account_keys
+                                .into_iter()
+                                .flat_map(|key_bytes| {
+                                    let slice: &[u8] = key_bytes.as_slice();
+                                    Pubkey::try_from(slice).ok()
+                                })
+                                .collect();
 
-                            // blacklisted txs appear twice
 
                             // info!("sig {} - bl {}", tx_sig, is_blacklist);
 
                             tx_count_total += 1;
                             if is_blacklist {
                                 tx_count_blacklisted += 1;
+
+                                // note: this is incomplete due to missing ALT lookup
+                                let blacklist_item = account_keys.iter()
+                                    .filter(|pubkey| blacklist.contains(pubkey))
+                                    .collect_vec();
+
+                                // info!("slot {} - blacklisted tx sig {} matching {:?}", slot, tx_sig, blacklist_item);
+
+                                for pk in blacklist_item {
+                                    *blacklisted_by_account.entry(*pk).or_insert(0) += 1;
+                                }
+                            }
+                            if is_phoenix {
+                                tx_count_phoenix += 1;
                             }
 
                             // dump
                             if slot > last_print + 10 {
                                 last_print = slot;
 
-                                info!("slot: {}, total txs: {}, blacklisted txs: {}, % blacklisted: {:.0}%",
-                                    slot, tx_count_total, tx_count_blacklisted, (tx_count_blacklisted as f64 / tx_count_total as f64) * 100.0);
+                                info!("slot: {}, total txs: {}, blacklisted txs: {}, phoenix: {}, % blacklisted: {:.0}%",
+                                    slot, tx_count_total, tx_count_blacklisted, tx_count_phoenix, (tx_count_blacklisted as f64 / tx_count_total as f64) * 100.0);
+
+                                info!("Top blacklisted accounts:");
+                                for (pk, count) in blacklisted_by_account.iter().sorted_by(|a, b| b.1.cmp(a.1)).take(10) {
+                                    info!("  {} - {}", pk, count);
+                                }
                             }
                         }
                         None => {}
@@ -134,15 +162,52 @@ fn start_tracking_account_consumer(
 }
 
 
-fn all_transactions() -> SubscribeRequest {
+// note that Vote accounts are excluded
+fn filter_transactions() -> SubscribeRequest {
     let mut transactions_subs = HashMap::new();
+
+
     transactions_subs.insert(
         "transaction_all".to_string(),
         SubscribeRequestFilterTransactions {
-            vote: None,
+            vote: Some(false),
             failed: None,
             signature: None,
             account_include: vec![],
+            account_exclude: vec![],
+            account_required: vec![],
+        },
+    );
+
+
+    let account_blacklist = blacklist();
+    transactions_subs.insert(
+        "transaction_sub_blacklisted".to_string(),
+        SubscribeRequestFilterTransactions {
+            vote: Some(false),
+            failed: None,
+            signature: None,
+            // note: filter INCLUDES the unwanted transactions; for real use, one would EXCLUDE them
+            account_include: account_blacklist.iter().map(|s| s.to_string()).collect(),
+            account_exclude: vec![],
+            account_required: vec![],
+        },
+    );
+
+
+    let account_phoenix = vec![
+        "4DoNfFBfF7UokCC2FQzriy7yHK6DY6NVdYpuekQ5pRgg",
+        "Ew3vFDdtdGrknJAVVfraxCA37uNJtimXYPY4QjnfhFHH",
+        // TODO move
+
+    ];
+    transactions_subs.insert(
+        "transaction_sub_phoenix".to_string(),
+        SubscribeRequestFilterTransactions {
+            vote: Some(false),
+            failed: None,
+            signature: None,
+            account_include: account_phoenix.iter().map(|s| s.to_string()).collect(),
             account_exclude: vec![],
             account_required: vec![],
         },
@@ -155,9 +220,9 @@ fn all_transactions() -> SubscribeRequest {
 }
 
 
-fn blacklisted_transactions() -> SubscribeRequest {
 
-    let account_blacklist = vec![
+fn blacklist() -> Vec<&'static str> {
+    vec![
         "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
         "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
         // OpenBook
@@ -180,27 +245,9 @@ fn blacklisted_transactions() -> SubscribeRequest {
         // BPF Loader
         "BPFLoader2111111111111111111111111111111111",
         "BPFLoaderUpgradeab1e11111111111111111111111",
-        // Vote program
+        // Vote program - actually not relevant
         "Vote111111111111111111111111111111111111111",
         // Raydium Concentrated Liquidity
         "CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK",
-    ];
-
-    let mut transactions_subs = HashMap::new();
-    transactions_subs.insert(
-        "transaction_sub_blacklisted".to_string(),
-        SubscribeRequestFilterTransactions {
-            vote: None,
-            failed: None,
-            signature: None,
-            account_include: vec![],
-            account_exclude: account_blacklist.iter().map(|s| s.to_string()).collect(),
-            account_required: vec![],
-        },
-    );
-
-    SubscribeRequest {
-        transactions: transactions_subs,
-        ..Default::default()
-    }
+    ]
 }
